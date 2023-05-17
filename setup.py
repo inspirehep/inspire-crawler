54352014af4df8f3fd030d047c4883ce56db9b68
MJedr <jedrych.marcysia@gmail.com>
2023-05-17 11:30:59 +0200

    pin invenio workflows

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ install_requires = [
     'pathlib2>=2.1.0',
     'invenio-celery>=1.0.0a3',
     'celery~=4.0,>=4.1.0',
-    'invenio_workflows~=7.0',
+    'invenio_workflows==7.0',
     'invenio_workflows_ui~=2.0.1',
     'invenio_oaiharvester>=1.0.0a3',
 ]
